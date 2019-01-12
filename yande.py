7221b2ba96f239ddc514d2cc96d36ca2022e34b4
Starman <jxxclj@gmail.com>
2019-01-12 14:45:56 +0800

    Update yande.py

--- yande.py
+++ yande.py
@@ -1,19 +1,21 @@
-import os#引入文件模块
-import re#正则表达式
+import os  # 引入文件模块
+import re  # 正则表达式
 import urllib.request
+import threading
 
-#连接网页并返回源码
+# 连接网页并返回源码
 def open_url(url):
-      try:
-            req = urllib.request.Request(url)
-            req.add_header("User-Agent", "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36")
-            response = urllib.request.urlopen(req)
-            status_code = response.code
-            html = response.read()
-            return html
-      except:
-            print(url + " 404")
-            return 404
+    try:
+        req = urllib.request.Request(url)
+        req.add_header("User-Agent",
+                       "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/55.0.2883.87 Safari/537.36")
+        response = urllib.request.urlopen(req)
+        status_code = response.code
+        html = response.read()
+        return html
+    except:
+        print(url + " 404")
+        return 404
 
 def mkdir(path):
     '''
@@ -45,35 +47,76 @@ def mkdir(path):
         return False
 
 
-def Yande1():
+def Yande1(i):
+    imgs = 1
+    url = 'https://yande.re/post?page=' + str(i)
+    floder = "E:\\Python\\爬虫\\yande\\img\\page" + str(i)
+    mkdir(floder)
+
+    html = open_url(url)
+    html = html.decode('gbk', 'ignore')
+    img_adds = []
+    img_adds = re.findall(r'<a class="directlink largeimg" href="([^"]+\.jpg)"', html)
+    for i in img_adds:
+        filename = floder + "\\" + str(imgs) + '.jpg'
+        imgs += 1
+        img_html = open_url(i)
+        if img_html == 404:
+            continue
+        with open(filename, 'wb') as f:
+            f.write(img_html)
+            print(i + ' 下载完成......')
+
+exitflag = 0
+
+class myThread(threading.Thread):
+    def __init__(self, threadID, name, list):
+        threading.Thread.__init__(self)
+        self.threadID = threadID
+        self.name = name
+        self.list = list
+    def run(self):
+        print("开始线程：" + self.name)
+        # threadLock.acquire()
+        get_img(self.name, self.list)
+        # threadLock.release()
+        print("退出线程："+ self.name)
+
+
+def get_img(threadname, list):
+    if len(list):
+        for i in list:
+            if exitflag:
+                threadname.exit()
+            Yande1(i)
+
+if __name__ == '__main__':
     pages1 = int(input('请输入你要下载的起始页面数：'))
     pages2 = int(input('请输入你要下载的末尾页面数：'))
     mkdir('img')
-    for i in range(pages1,pages2+1):
-        imgs = 1
-        print(i)
-        url = 'https://yande.re/post?page='+str(i)
-        #print(url)
-        floder = "E:\Python\爬虫\yande\img\page"+str(i)
-        mkdir(floder)
-        os.chdir(floder)
 
-        html = open_url(url)
-        html = html.decode('gbk', 'ignore')
-        #print(html)
-        img_adds = []
-        img_adds = re.findall(r'<a class="directlink largeimg" href="([^"]+\.jpg)"', html)
-        for i in img_adds:
-            filename = str(imgs)+'.jpg'
-            imgs += 1
-            img_html = open_url(i)
-            if img_html == 404:
-                continue
-            with open(filename, 'wb') as f:
-                f.write(img_html)
-                print(i+'下载完成....')
-        #print(len(img_adds))
+    list1 = []
+    list2 = []
+    list3 = []
+    for i in range(pages1, pages2+1):
+        if i % 3 == 0:
+            list3.append(i)
+        if i % 3 == 1:
+            list1.append(i)
+        if i % 3 == 2:
+            list2.append(i)
+    threadLock = threading.Lock()
+    threads = []
+    thread1 = myThread(1, "thread-1", list1)
+    thread2 = myThread(2, "thread-2", list2)
+    thread3 = myThread(3, "thread-3", list3)
+    thread1.start()
+    thread2.start()
+    thread3.start()
+    threads.append(thread1)
+    threads.append(thread2)
+    threads.append(thread3)
 
-
-if __name__ == '__main__':
-    Yande1()
+    for t in threads:
+        t.join()
+    print("退出主线程")
